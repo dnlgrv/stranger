d6b3712726743b3a2ffe38ebaf6553c2a2edbe46
Daniel Grieve <git@cazrin.net>
2016-01-23 21:18:35 +0000

    Add GA tracking code in production

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Stranger.Mixfile do
 
   def project do
     [app: :stranger,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
