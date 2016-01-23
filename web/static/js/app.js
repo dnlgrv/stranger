fb966ab0c0aea287f495c8a0bd88514c5b2e9f38
Daniel Grieve <git@cazrin.net>
2016-01-23 13:09:52 +0000

    Add welcome dialog to agree to user agreement

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,5 +2,21 @@ import "phoenix_html"
 import Chat from "./chat"
 
 if(document.querySelector("[data-init='chat']")) {
-  new Chat()
+  if(localStorage.getItem("agreed")) {
+    new Chat()
+  } else {
+    showWelcome()
+  }
+}
+
+function showWelcome() {
+  const el = document.querySelector(".welcome")
+  el.classList.add("welcome--show")
+
+  const startButton = el.querySelector("[data-click='start']")
+  startButton.addEventListener("click", e => {
+    el.classList.remove("welcome--show")
+    localStorage.setItem("agreed", true)
+    new Chat()
+  })
 }
