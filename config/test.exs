62ff5d28ece5701e42c08b6ead2a60d02ab5d993
Daniel Grieve <git@cazrin.net>
2016-01-25 23:31:35 +0000

    Add view tests for helpers

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,8 @@
 use Mix.Config
 
+config :stranger,
+  ga_tracking_code: "test"
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :stranger, Stranger.Endpoint,
