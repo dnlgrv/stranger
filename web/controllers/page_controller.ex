ce7e1a9d29df67ac61c08dffe04a9f9d1b87f7cb
Daniel Grieve <git@cazrin.net>
2016-01-26 14:34:41 +0000

    random_id doesn't need an arg

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,14 +4,14 @@ defmodule Stranger.PageController do
   @id_length Application.get_env(:stranger, :id_length)
 
   def index(conn, _params) do
-    render conn, "index.html", id: random_id(@id_length)
+    render conn, "index.html", id: random_id()
   end
 
   def agreement(conn, _params) do
     render conn, "agreement.html"
   end
 
-  defp random_id(length) do
+  defp random_id do
     :crypto.strong_rand_bytes(@id_length) |> Base.encode64()
   end
 end
