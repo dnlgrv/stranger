887dfe52c8b934cf6fd0d598d64c3735da96d4e1
Daniel Grieve <git@cazrin.net>
2016-02-21 12:08:17 +0000

    Minor version increase for heroku config

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Stranger.Mixfile do
 
   def project do
     [app: :stranger,
-     version: "0.0.9",
+     version: "0.1.0",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
